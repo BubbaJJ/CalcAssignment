7d03b042df1a66b8c2b3b65c367ff8f082195b71
Jim Kaya <Jim.kaya@gmail.com>
2021-12-03 15:23:42 +0100

    Add project files.

---
+++ CalcAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CalcAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+
+            -- INSTRUKTIONER --
+            Samtliga metoder ska skapas i klassen Calculator.
+            Se CalcAssignmentTests.cs för vad metodnamn.
+            Testerna får inte modifieras.
+
+            -- KRAV --
+            Samtliga tester ska vara godkända vid inlämning.
+
+            -- OLIKA SÄTT ATT STARTA TESTERNA --
+
+            - Alt 1 -
+            Högerklicka på CalcInl1
+            Välj "Run Tests".
+
+            - Alt 2 -
+            Klicka på Test i menyn längst upp.
+            Välj "Run All Tests".
+
+            - Alt 3 -
+            Håll inne CTRL och tryck på R följt av A.
+
+            */
+        }
+    }
+}
---
+++ CalcAssignmentTests/CalculatorTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalcAssignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcAssignment.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            var calc = new Calculator();
+
+            double expected = 25;
+            double result = calc.Add(10, 15);
+            Assert.AreEqual(expected, result);
+
+            expected = 12.5;
+            result = calc.Add(10, 2.5);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            var calc = new Calculator();
+
+            double expected = 10;
+            double result = calc.Subtract(15, 5);
+            Assert.AreEqual(expected, result);
+
+            expected = 73.2;
+            result = calc.Subtract(80.5, 7.3);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            var calc = new Calculator();
+
+            double expected = 10;
+            double result = calc.Multiply(5, 2);
+            Assert.AreEqual(expected, result);
+
+            expected = 4.8;
+            result = calc.Multiply(2.4, 2);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            var calc = new Calculator();
+
+            double expected = 5;
+            double result = calc.Divide(10, 2);
+            Assert.AreEqual(expected, result);
+
+            expected = 7.5;
+            result = calc.Divide(22.5, 3);
+            Assert.AreEqual(expected, result);
+
+            result = calc.Divide(5, 0);
+            Assert.IsTrue(double.IsInfinity(result));
+        }
+    }
+}
